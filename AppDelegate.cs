ca2e628d0dcfcdbf3bc912bd98b121c696316ee3
Sherry Hsia <shsia>
2013-06-24 22:59:58 +1000

    test commit changes
    test commit changes

--- AppDelegate.cs
+++ AppDelegate.cs
@@ -7,6 +7,8 @@ using MonoTouch.UIKit;
 
 namespace Test
 {
+
+	//test la
 	// The UIApplicationDelegate for the application. This class is responsible for launching the 
 	// User Interface of the application, as well as listening (and optionally responding) to 
 	// application events from iOS.
